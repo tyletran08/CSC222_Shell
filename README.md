b6fdc6ac47fec2f7381f198befe4913891d73db0
Tyler Tran <tyletran08@gmail.com>
2023-10-23 17:07:33 -0500

    Updated main.c and README.md to be more inline with Developmental branch

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
+# This is the Developmental Branch, used to put new things and break things!
+
 # CSC222 Final Project
+
 ### Tyler Tran, Dalton Michiels, Colby Women
 
 This is our final project for CSC222 which is making a shell!
 
-### This is the Developmental Branch, used to put new things and break things!
 ---
+
 ## Table of Contents
 * [Summary](#Summary)
 * [Features](#Features)
--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 /*
+This is the Developmental Branch, used to put new things and break things!
+
+Git stuff:
+After edits, save file and do "git add ."
+Then do "git commit -m "message""
+Then do "git push origin developmental"
+
+Regular C stuff:
+Save file and run "gcc main.c -o shell"
+To run, do "./shell"
+*/
+
+/*
 Author: Tyler Tran, Dalton Michiels, Colby Women 
 Date: 10-23-2023
 Description: CSC222 Programming Project - A small shell with I/O redirections
-
-This is the Developmental Branch, used to put new things and break things!
-
-Remember to save and "gcc main.c -o shell"
-To run, do "./shell"
 */
 
 #include <stdio.h>
