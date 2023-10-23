83fbe5c0094c0beb9619cca75777908f33c94b3b
Tyler Tran <tyletran08@gmail.com>
2023-10-23 15:02:46 -0500

    Added main.c, default main.c status, and updated README.md

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+Author: Tyler Tran, Dalton Michiels, Colby Women 
+Date: 10-23-2023
+Description: CSC222 Programming Project - A small shell with I/O redirections
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
