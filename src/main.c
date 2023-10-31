3bd68cadecca52537ad981571e0c39c918e10728
Tyler Tran <97276727+tyletran08@users.noreply.github.com>
2023-10-31 16:41:58 -0500

    Update main.c
    Made changes to main.c to reflect main branch

--- src/main.c
+++ src/main.c
@@ -1,17 +1,4 @@
 /*
-This is the Developmental Branch, used to put new things and break things!
-
-Git stuff:
-After edits, save file and do "git add ."
-Then do "git commit -m "message""
-Then do "git push origin Developmental"
-
-Regular C stuff:
-Save file and run "gcc main.c -o shell"
-To run, do "./shell"
-*/
-
-/*
 Author: Tyler Tran, Dalton Michiels, Colby Women 
 Date: 10-23-2023
 Description: CSC222 Programming Project - A small shell with I/O redirections
